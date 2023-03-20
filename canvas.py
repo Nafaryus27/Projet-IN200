9d8255217b638acbe27acc0e54e1c82327a89987
MARIAMA MANGANE <mariama.mangane@ens.uvsq.fr>
2023-03-20 15:31:37 +0100

    amélioration fonction changer couleur

--- canvas.py
+++ canvas.py
@@ -1,11 +1,24 @@
-def dessiner_grille(can, n, s):
-    indices=[[0 for i in range(n)] for j in range(n)]
-    for i in range(n):
-        for j in range(n):
+
+
+import tkinter as tk
+#creation d'un dictionnaire pour stocker les rectangles numerotés et leur coordonnées
+
+def grille(Can,n,s): #definition d'une grille 
+    
+    for i in range(1,n): #on met à 1 pour avoir les rectangles bien numerotés
+        for j in range(1,n):
             
             x=(i+1) * s
             y=(j+1) * s
             
-            indices[i][j] = can.create_rectangle(i*s,j*s,x,y,outline="black")
+            Can.create_rectangle(i*s,j*s,x,y,outline="black")
+    return
 
-    return indices
+        
+
+### fonction pour changer la couleur d'un rectangle
+
+def changer_couleur(Can,h,x,y,couleur):
+    Can.itemconfigure(x*h+y+1,fill = couleur)   #Can.itemconfigure
+    
+changer_couleur(Can,5,8,"red")
