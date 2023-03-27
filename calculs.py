8baf0fd7617685b5ebe17ef3b389545af5803dff
Fernanda CARBAJAL <fernanda.carbajal@ens.uvsq.fr>
2023-03-27 13:53:00 +0200

    Utilisation modulo pour remplacer les coordonnées

--- calculs.py
+++ calculs.py
@@ -1,25 +1,38 @@
-def bouger(pos, dir, items): #position et direction (cardinaux)
-    i, j = pos  #position par rapport a ses coordonnées (lignes, colonnes)
+def bouger(pos, dir, items,n): #position et direction (cardinaux)
+    y, x = pos  #position par rapport a ses coordonnées (lignes, colonnes)
 
-    if items[i][j] == 0: #si la couleur de la case est claire
+    if items[y][x] == 0: #si la couleur de la case est claire
         if dir == "N":
-            r = (i, j + 1), "E" "est"
+            x += 1
+            ndir = "E" #est
         elif dir == "S":
-            r = (i, j - 1), "W" "ouest"
+            x -= 1
+            ndir="W" #ouest
         elif dir == "E":
-            r = (i + 1, j), "S" "sud" #une ligne en dessous et la même colonne
+            y += 1
+            ndir= "S" #sud #une ligne en dessous et la même colonne
         elif dir == "W":
-            r = (i - 1, j), "N" "nord"
+            y -= 1
+            ndir= "N" #nord
+        items[y][x] = 1
     else:  #case sombre
         if dir == "S":  
-            r = (i, j + 1), "E"  
+            x += 1
+            ndir= "E"  
         elif dir == "N":
-            r = (i, j - 1), "W"
+            x -= 1
+            ndir= "W"
         elif dir == "W":
-            r = (i + 1, j), "S" 
+            y += 1
+            ndir= "S" 
         elif dir == "E":
-            r = (i - 1, j), "N"
-    return r #r est un couple (npos (nouvelle position couple ligne x colonne), ndir (nouvelle direction (donc parmi "N", "S", "E" et "W"))
+            y -= 1
+            ndir= "N"
+        items[y][x] = 0
+    y %= n
+    x %= n
+
+    return x,y,ndir #r est un couple (npos (nouvelle position couple ligne x colonne), ndir (nouvelle direction (donc parmi "N", "S", "E" et "W"))
 
 
-#Par exemple, si i=42, j=81 , dir="W" et la case (i, j) est noire alors la fourmi se déplace à sa gauche (donc vers la bas), donc sa nouvelle position est (42, 82) et sa nouvelle direction est "S".
+#Par exemple, si y=42, x=81 , dir="W" et la case (y, x) est noire alors la fourmi se déplace à sa gauche (donc vers la bas), donc sa nouvelle position est (42, 82) et sa nouvelle direction est "S".
