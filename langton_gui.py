5f0f3a25981129097458d3d52c225318f24d12d9
Nathan GILLES--LORDET <nafaryus27@gmail.com>
2023-03-31 09:30:27 +0200

    Début restructuration selon un schéma Controller-Model-View

--- langton_gui.py
+++ langton_gui.py
@@ -1,9 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
+from viewer import Viewer
 
-
-class LangtonsAntApp(tk.Tk):
-
+class LangtonGUI(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         self.title("Langton's ant")
@@ -11,121 +10,145 @@ class LangtonsAntApp(tk.Tk):
 
         style = ttk.Style(self)
         style.theme_use('clam')        
-        
         self.option_add('*tearOff', False)
-        menu_bar = MenuBar(self)
-        self['menu'] = menu_bar
-
+        
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
+        self.init_widgets()
+
+    def init_widgets(self):
+        menu_bar = MenuBar(self)
+        self['menu'] = menu_bar
+    
         main_frame = ttk.Frame(self)
         main_frame.pack(side="top", fill="both", expand=True)
-
         main_frame.grid_columnconfigure(0, weight=1)
         main_frame.grid_rowconfigure(0, weight=1)
 
-        self.pages = {}
+        ctrl_start_page = StartPageCTRL(self, model),
+        ctrl_simulation_view = SimulationViewCTRL(self, model)
         
-        for F in (StartupPage, SimulationView):
-            frame = F(main_frame, self)
-            self.pages[F] = frame
-            
-            frame.grid(row=0, column=0, sticky="news")
 
-        self.show(StartupPage)
+class StartPageCTRL:
+    def __init__(self, parent, model):
+        self.view = StartPage(parent, self)
+        self.model = model
 
-    def show(self, p):
-        page = self.pages[p]
-        if p == SimulationView:
-            w,h,r = self.pages[StartupPage].get_input_values()
-            page.launch(w,h,r)
-        page.tkraise()
+    def show(self):
+        self.view.tkraise()
+        
+    def launch_callback(self):
+        pass
 
+    def get_rule(self):
+        return self.view.rule_entry.get_rule()
 
-class StartupPage(ttk.Frame):
+        
+class StartPage(ttk.Frame):
     def __init__(self, parent, controller):
         ttk.Frame.__init__(self, parent)
-        
 
+        self.controller = controller
+        
         self.grid_rowconfigure(0, weight=1)
         self.grid_rowconfigure(2, weight=1) 
-
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(2, weight=1)
+        self.grid(row=0, column=0, sticky="news")
+       
+        self.init_widgets()
 
+
+    def init_widgets(self):
         cont = ttk.Frame(self)
         cont.grid(row=1, column=1)
 
-        self.rule_label = ttk.Label(cont, text="Rule")
-        self.rule_label.grid(row=0, column=0, sticky='e')
-        self.rule_val = tk.StringVar(self)
-        self.rule_val.set("RL")
-        self.rule_input = ttk.Entry(cont, textvariable=self.rule_val)
-        self.rule_input.grid(row=0, column=1)
-
+        self.rule_entry = LabeledEntry(cont, "Rule", "RL")
         
-        launch_button = ttk.Button(cont, text="Launch", command=lambda : controller.show(SimulationView))
-        launch_button.grid(row=3, column=0, columnspan=2, sticky='news')
-
+                
+        launch_button = ttk.Button(cont, text="Launch", command=self.controller.launch_callback)
         load_button = ttk.Button(cont, text="Load", command=load_file)
-        load_button.grid(row=4, column=0, columnspan=2, sticky='news')
 
-    def get_input_values(self):
-        return(self.rule_input.get())
-
-
-class LabeledEntry(ttk.Frame):
-    def __init__(self, parent, text:str, default_value:str):
-        ttk.Frame.__init__(self, parent)
-
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=1)
-        
+        rule_entry.grid(row=0, column=0, sticky='news')
+        launch_button.grid(row=3, column=0, sticky='news')
+        load_button.grid(row=4, column=0, sticky='news')
 
 
-    def get(self):
-        return(self._input.get())
+class SimulationViewCTRL:
+    def __init__(self, parent, controller):
+        self.view = SimulationView(parent, self)
+        self.model = model
+    
+    def sim_next(self):
+        pass
 
-    def set(self, value):
-        self.val.set(value)
+    def sim_previous(self):
+        pass
+
+    def sim_play(self):
+        pass
+
+    def sim_pause(self):
+        pass
+
+    def sim_reset(self):
+        pass
 
     
 class SimulationView(ttk.Frame):
     def __init__(self, parent, controller):
         ttk.Frame.__init__(self, parent)
 
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(1, weight=1)
+        self.controller = controller
 
-        tool_bar = ToolBar(self, relief='ridge')
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+        
+        tool_bar = ToolBar(self)
         tool_bar.grid(row=0, column=0, sticky='ew')
 
-    def launch(self, w, h, r):
-        view = Viewer(self, bg='black')
-        view.grid(row=1, column=0, sticky='news')
-
-    
+        self.viewer = Viewer(self, 900, 900, 15)
+        self.viewer.grid(row=1, column=1, sticky='news')
 
         
-class Viewer(tk.Canvas):
-    def __init__(self, *args, **kwargs):
-        tk.Canvas.__init__(self, *args, **kwargs)
+class LabeledEntry(ttk.Frame):
+    def __init__(self, master, text:str, default_value:str):
+        ttk.Frame.__init__(self, master)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
+        self.value = tk.StringVar(self, default_value)
+
+        self.init_widgets(text, default_value)
         
+    def init_widgets(self, text:str, default_value:str):
+        label = ttk.Label(self, text=text)
+        label.grid(row=0, column=0, sticky='e')
+        
+        entry = ttk.Entry(self, textvariable=self.value)
+        entry.grid(row=0, column=1)
+        
+        self.get = entry.get
 
                       
 class ToolBar(ttk.Frame):
-    def __init__(self, *args, **kwargs):
-        ttk.Frame.__init__(self, *args, **kwargs)
+    def __init__(self, parent):
+        ttk.Frame.__init__(self, parent)
         
         button = ttk.Button(self, text="Test")
         button.grid()
 
+        
 class MenuBar(tk.Menu):
+    def __init__(self, master):
+        tk.Menu.__init__(self, master)
 
-    def __init__(self, *args, **kwargs):
-        tk.Menu.__init__(self, *args, **kwargs)
-
+        self.controller = master
+        self.init_widgets()
+        
+    def init_widgets(self):
         menu_file = tk.Menu(self)
         menu_file.add_command(label="New", command=new_file)
         menu_file.add_command(label="Load", command=load_file)
@@ -133,7 +156,7 @@ class MenuBar(tk.Menu):
         menu_file.add_command(label="Close", command=close_file)
         menu_file.add_separator()
         menu_file.add_command(label="Settings", command=settings)
-        menu_file.add_command(label="Quit", command=args[0].destroy)
+        menu_file.add_command(label="Quit", command=self.master.destroy)
 
         self.add_cascade(menu=menu_file, label="File")
 
@@ -152,8 +175,6 @@ class MenuBar(tk.Menu):
         menu_help.add_command(label="Documentation", command=documentation)
         menu_help.add_command(label="About", command=about)
         self.add_cascade(menu=menu_help, label="Help")
-
-
 
 
 def new_file():
@@ -176,25 +197,20 @@ def settings():
     pass
 
 
-def sim_next():
+def sim_next(self):
     pass
 
-
-def sim_previous():
+def sim_previous(self):
     pass
 
-
-def sim_play():
+def sim_play(self):
     pass
 
-
-def sim_pause():
+def sim_pause(self):
     pass
 
-
-def sim_reset():
+def sim_reset(self):
     pass
-
 
 def documentation():
     pass
